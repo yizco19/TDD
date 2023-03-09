cb0be65904b3b5da68a9983e5185cde22f37b38b
Yang <zhiyang520679@gmail.com>
2023-03-09 19:00:24 +0100

    Test suma tres numeros

--- Tareas-TDD/Test/FizzBuzzTest.java
+++ Tareas-TDD/Test/FizzBuzzTest.java
@@ -4,8 +4,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
     @Test
-    void testSum(){
+    void testSum1(){
         FizzBuzz f=new FizzBuzz();
         assertEquals(2,f.suma(1,1));
+    }
+    @Test
+    void testSum2(){
+        FizzBuzz f=new FizzBuzz();
+        assertEquals(4,f.suma(1,1,2));
     }
 }
--- Tareas-TDD/src/FizzBuzz.java
+++ Tareas-TDD/src/FizzBuzz.java
@@ -4,5 +4,8 @@ public class FizzBuzz {
     int suma(int num1,int num2){
         return num1+num2;
     }
+    int suma(int num1,int num2,int num3){
+        return num1+num2+num3;
+    }
 
 }
