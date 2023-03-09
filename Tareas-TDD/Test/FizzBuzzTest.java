ff815dac4d7256e265337874332be8b39ed67ee8
Yang <zhiyang520679@gmail.com>
2023-03-09 19:41:55 +0100

    Test 2 FizzBuzz

--- Tareas-TDD/Test/FizzBuzzTest.java
+++ Tareas-TDD/Test/FizzBuzzTest.java
@@ -10,5 +10,13 @@ class FizzBuzzTest {
         assertEquals("1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n", resultado);
     }
 
+    @Test
+    public void testFizzBuzzConMaximoCero() {
+        FizzBuzz fb = new FizzBuzz();
+        String resultado = fb.jugar(0);
+        assertEquals("", resultado);
+    }
+
+
 
 }
